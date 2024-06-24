b5e149905f9c2fd618db3eb3b51ba727eee8dd19
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-06-24 10:04:45 -0300

    ended draft

--- SimulacaoBancaria.java
+++ SimulacaoBancaria.java
@@ -1,11 +1,19 @@
 import java.util.Scanner;
+
 public class SimulacaoBancaria {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in); //cria o input do usuário
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         double saldo = 0;
         boolean continuar = true;
 
+        System.out.println("Bem-vindo(a) à Simulação Bancária!");
+
         while (continuar) {
+            System.out.println("\nOpções:");
+            System.out.println("1 - Depositar");
+            System.out.println("2 - Sacar");
+            System.out.println("3 - Consultar saldo");
+            System.out.println("0 - Encerrar o programa");
 
             int opcao = scanner.nextInt();
 
@@ -15,18 +23,23 @@ public class SimulacaoBancaria {
                     double valorDeposito = scanner.nextDouble();
                     saldo += valorDeposito; // Atualiza o saldo
                     System.out.println("Depósito realizado. Saldo atual: " + saldo);
-                    // TODO: Ler o valor a ser depositado e atualizar/imprimir o saldo.
-                    
                     break;
                 case 2:
-                    // TODO: Ler o valor a ser sacado e verificar/imprimir se há saldo suficiente.
+                    System.out.print("Digite o valor a ser sacado: ");
+                    double valorSaque = scanner.nextDouble();
+                    if (valorSaque <= saldo) {
+                        saldo -= valorSaque; // Atualiza o saldo
+                        System.out.println("Saque realizado. Saldo atual: " + saldo);
+                    } else {
+                        System.out.println("Saldo insuficiente para o saque.");
+                    }
                     break;
                 case 3:
-                    // TODO: Exibir o saldo atual da conta.
+                    System.out.println("Saldo atual: " + saldo);
                     break;
                 case 0:
                     System.out.println("Programa encerrado.");
-                    continuar = false;  // Atualiza a variável de controle para encerrar o loop
+                    continuar = false;
                     break;
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
