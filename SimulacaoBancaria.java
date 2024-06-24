daa4ce85347595c98a387e7de3a12a59d9214970
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-06-24 11:53:21 -0300

    ended challenge 1

--- SimulacaoBancaria.java
+++ SimulacaoBancaria.java
@@ -1,45 +1,42 @@
-import java.util;
 import java.util.Scanner;
 
 public class SimulacaoBancaria {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double saldo = 0.0;
+        double saldo = 0;
         boolean continuar = true;
 
         while (continuar) {
-           
-       
+            
             int opcao = scanner.nextInt();
 
             switch (opcao) {
                 case 1:
-                    System.out.print("Digite o valor a ser depositado: ");
                     double deposito = scanner.nextDouble();
-                    saldo = saldo + deposito;
-                    System.out.printf("Saldo atual: %.1f%n", saldo);
+                    saldo += deposito;
+                    System.out.printf("Saldo atual: %.1f\n", saldo);
                     break;
                 case 2:
-                    System.out.print("Digite o valor a ser sacado: ");
                     double saque = scanner.nextDouble();
-                    saldo = saldo - saque;
-                    if (saldo >= saque) {
-                       
-                        System.out.printf("Saldo atual: %.1f%n", saldo);
+                    if (saque <= saldo) {
+                        saldo -= saque;
+                        System.out.printf("Saldo atual: %.1f\n", saldo);
                     } else {
                         System.out.println("Saldo insuficiente.");
                     }
                     break;
                 case 3:
-                    System.out.printf("Saldo atual: %.1f%n", saldo);
+                    System.out.printf("Saldo atual: %.1f\n", saldo);
                     break;
                 case 0:
                     System.out.println("Programa encerrado.");
-                    return;
+                    continuar = false;
+                    break;
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
             }
         }
+        scanner.close();
     }
 }
