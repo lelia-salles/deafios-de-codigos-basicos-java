598ba1f3f8fbe48c38b61474a1e244703dfd291f
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-01 15:22:59 +0000

    updated

--- LendoPulandoNomes.java
+++ LendoPulandoNomes.java
@@ -1,21 +1,31 @@
 import java.util.Scanner;
+import java.util.Scanner;
 
 public class Problem {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	
+		
+// TODO: complete os espaços em branco com sua solução para o problema
+		String nomes[] = new String[10];
+		
+		for (int i = 0; i < 10; i++){
+		  nomes[1] = sc.nextLine(); 
+		  if (i == 2 || i == 7 || i == 9) {
+		
+			System.out.println(nomes[2]);
+	    System.out.println(nomes[6]);
+	    System.out.println(nomes[8]);
+		  }
+		  
+		  
+		}
+		
+		//for(int i=0 ; i<10 ; i++){
+		  
+		
 
-    
-    String[] nomes = new String[10];
-    System.out.println(nomes[2]);
-    System.out.println(nomes[6]);
-    System.out.println(nomes[8]);
-    
-    for (int i = 0; i < 10; i++) {
-      nomes[i] = sc.nextLine();
-    } 
-    
-    
-
-    sc.close(); 
-  }
+	//}
+	
 }
+  
