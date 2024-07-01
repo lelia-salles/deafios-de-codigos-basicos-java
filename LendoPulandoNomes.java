dad34b292905a282c3dedf8562035649945cfda4
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-01 14:35:50 +0000

    updated

--- LendoPulandoNomes.java
+++ LendoPulandoNomes.java
@@ -1,15 +1,20 @@
-// Abaixo segue um exemplo de código que você pode ou não utilizar
 import java.util.Scanner;
 
 public class Problem {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 // TODO: complete os espaços em branco com sua solução para o problema
-		String[] nomes = new String[USP, UFPE, UFCG, UFRN, UFRJ, IME, ITA, UNIOESTE, URI, UFG];
+		String[] nomes = new String[10];
+		System.out.println("Digite 10 nomes:");
+
 		
 		for(int i=0 ; i<10 ; i++)
-			nomes[ 2, 6, 8 ] = sc.nextLine();
-			println(nomes);
-
+			nomes[i] = sc.nextLine();
 	}
+	else("quantidade incorreta, por favor digite novamente");
+	
+	System.out.println(nomes[2]);
+	System.out.println(nomes[6]);
+	System.out.println(nomes[8]);
 }
+
