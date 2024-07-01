e99e5756b77786b54a2691d253919e1a2137c002
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-01 14:42:17 +0000

    solved merge

--- LendoPulandoNomes.java
+++ LendoPulandoNomes.java
@@ -1,36 +1,21 @@
 import java.util.Scanner;
 
 public class Problem {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-// TODO: complete os espaços em branco com sua solução para o problema
-<<<<<<< HEAD
-		String[] nomes = new String[10];
-		System.out.println("Digite 10 nomes:");
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-		
-		for(int i=0 ; i<10 ; i++)
-			nomes[i] = sc.nextLine();
-	}
-	else("quantidade incorreta, por favor digite novamente");
-	
-	System.out.println(nomes[2]);
-	System.out.println(nomes[6]);
-	System.out.println(nomes[8]);
-=======
-		String[] nomes = new String[   10  ];
-		
-		for(int i=0 ; nomes.lenght ; i++){
-		  System.out.println("digite 10 nomes em até 30 caracteres", i + 1);
-		  nomes[ i   ] = sc.nextLine();
-		}
-			
+    
+    String[] nomes = new String[10];
+    System.out.println(nomes[2]);
+    System.out.println(nomes[6]);
+    System.out.println(nomes[8]);
+    
+    for (int i = 0; i < 10; i++) {
+      nomes[i] = sc.nextLine();
+    } 
+    
+    
 
-	}
-	
-	System.out.println(nomes[2]);
-  System.out.println(nomes[6]);
-  System.out.println(nomes[8]);
->>>>>>> b61ad0e6cfe467ba7df2ee17568fed0b3b6c14be
+    sc.close(); 
+  }
 }
-
