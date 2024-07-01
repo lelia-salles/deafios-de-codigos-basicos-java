b61ad0e6cfe467ba7df2ee17568fed0b3b6c14be
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-01 11:13:37 -0300

    updated

--- LendoPulandoNomes.java
+++ LendoPulandoNomes.java
@@ -1,15 +1,21 @@
-// Abaixo segue um exemplo de código que você pode ou não utilizar
 import java.util.Scanner;
 
 public class Problem {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 // TODO: complete os espaços em branco com sua solução para o problema
-		String[] nomes = new String[USP, UFPE, UFCG, UFRN, UFRJ, IME, ITA, UNIOESTE, URI, UFG];
+		String[] nomes = new String[   10  ];
 		
-		for(int i=0 ; i<10 ; i++)
-			nomes[ 2, 6, 8 ] = sc.nextLine();
-			println(nomes);
+		for(int i=0 ; nomes.lenght ; i++){
+		  System.out.println("digite 10 nomes em até 30 caracteres", i + 1);
+		  nomes[ i   ] = sc.nextLine();
+		}
+			
 
 	}
+	
+	System.out.println(nomes[2]);
+  System.out.println(nomes[6]);
+  System.out.println(nomes[8]);
 }
+
