a5c87228b135d3470f5c01f2fc0fb10352093b8a
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-05 14:14:22 +0000

    updated

--- VerificadorNumeroConta.java
+++ VerificadorNumeroConta.java
@@ -8,13 +8,15 @@ public class VerificadorNumeroConta {
         try {
             String numeroConta = scanner.nextLine();
 
-            // TODO: Chamar o método que verifica se o número da conta é valido
+            
+            // Chamar o método que verifica se o número da conta é valido
             verificarNumeroConta(numeroConta);
             
             // Caso nenhuma exceção seja lançada, imprime a mensagem de sucesso.
             System.out.println("Numero de conta valido.");
         } catch (IllegalArgumentException e) {
-            // TODO: Informar que o número de conta é inválido e exibir a mensagem de erro
+            
+            //  Informar que o número de conta é inválido e exibir a mensagem de erro
             System.out.println("Erro: " + e.getMessage());
         } finally {
             // Fechar o scanner para evitar vazamentos de recursos
@@ -24,7 +26,7 @@ public class VerificadorNumeroConta {
 
     private static void verificarNumeroConta(String numeroConta) {
         if (numeroConta.length() != 8) {
-            // TODO: Lançar uma IllegalArgumentException com a seguinte mensagem:
+         // Lançar uma IllegalArgumentException com a seguinte mensagem:
             // "Numero de conta invalido. Digite exatamente 8 digitos."
             throw new IllegalArgumentException("Número de conta inválido. Digite exatamente 8 dígitos.");
         }
