01b91809acfe86ca27cd40bb0e843ae36436abdf
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-07-15 17:51:08 +0000

    updated

--- PedraPapelTesoura.java
+++ PedraPapelTesoura.java
@@ -1,11 +1,11 @@
 // Abaixo segue um exemplo de código que você pode ou não utilizar
 import java.util.*; 
-public class Problem {
+public class PedraPapelTesoura {
     public static void main(String[] args) {
       	Scanner leitor = new Scanner(System.in);
     	int N = leitor.nextInt();
     	String jogador1, jogador2;
-// TODO: complete os espaços em branco com sua solução para o problema
+//  complete os espaços em branco com sua solução para o problema
     	for (int i = 0; i < N; i++) {
     		jogador1 = leitor.next();
     		jogador2 = leitor.next();
